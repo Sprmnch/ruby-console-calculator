5002e6b41d96e3511ee76d09b67b208b4c208b20
Christian de la Cruz <cdelacruzmunoz@palmertrinity.org>
2015-07-24 13:27:14 +0000

    Finished basic form

--- ruby-console-calculator.rb
+++ ruby-console-calculator.rb
@@ -21,3 +21,27 @@ end
 def modulus(num1, num2)
   num1 % num2
 end
+
+while true
+  puts "What would you like to calculate?"
+  math1 = gets.chomp
+  puts "What would you like to do with the number?"
+  math_operation = gets.chomp
+  puts "What other number would you like to use?"
+  math2 = gets.chomp
+  if math_operation == "+"
+    puts add(math1, math2)
+  elsif math_operation == "-"
+    puts subtract(math1, math2)
+  elsif math_operation == "*"
+    puts multiply(math1, math2)
+  elsif math_operation == "/"
+    puts divide(math1,math2)
+  elsif math_operation == "**"
+    puts power(math1,math2)
+  elsif math_operation == "%"
+    puts modulus(math1,math2)
+  else
+    puts "That is not right!"
+  end
+end
